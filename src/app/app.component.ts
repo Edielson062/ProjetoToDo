8782185c9c38bf5244b97fe4392f6d111cb2cbff
edielson <edielsongomes2712@gmail.com>
2025-04-03 18:56:52 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projetoToDo';
+  listaDeAlunos:string[] = [];
+  nomeAluno :string = "";
+
+  adicionarAluno(){
+    this.listaDeAlunos.push(this.nomeAluno);
+  }
 }
